1f2a56b57bdcbafb0cf3d931135de08eb16a8990
Leonardo Ribeiro <leonardotsr@gmail.com>
2020-05-25 09:06:43 +0200

    POC flow (WIP)

--- src/components/Quiz.vue
+++ src/components/Quiz.vue
@@ -1,6 +1,11 @@
 <template>
   <v-container>
-    <h1>{{ quiz.title }}</h1>
+    <div class="d-flex">
+      <h1 class="flex-grow-1">{{ quiz.title }}</h1>
+      <v-btn color="primary" @click="showPartyInviteDialog = true">
+        Create Party!
+      </v-btn>
+    </div>
     <v-card
       class="mx-auto mt-5"
       v-for="question in quiz.questions"
@@ -8,7 +13,7 @@
     >
       <v-card-title>{{ question.question }}</v-card-title>
       <v-card-text>
-        <ol type="a">
+        <ol type="a" v-if="question.answers.length">
           <li
             v-for="answer in question.answers"
             :key="answer.id"
@@ -18,14 +23,30 @@
             <v-icon v-if="answer.correct" color="pink">mdi-star</v-icon>
           </li>
         </ol>
+        <p v-else class="body-1 my-2 text-center">
+          &lt;&lt; Freeform answer &gt;&gt;
+        </p>
       </v-card-text>
     </v-card>
+    <v-dialog v-model="showPartyInviteDialog">
+      <PartyInvite />
+    </v-dialog>
   </v-container>
 </template>
 
 <script>
+import PartyInvite from './PartyInvite';
+
 export default {
   name: 'Quiz',
+  components: {
+    PartyInvite,
+  },
+  data() {
+    return {
+      showPartyInviteDialog: false,
+    };
+  },
   computed: {
     quiz() {
       return this.$store.state.quiz;
